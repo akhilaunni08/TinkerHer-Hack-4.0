ab3bf620a0cf052ef8a67550973e743fe00143ab
akhilaunni08 <vv1234akhila@gmail.com>
2026-02-14 06:03:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -179,4 +179,5 @@ def report():
     return render_template("report.html", report_data=report_data)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
